6129e8a1e22275c1bf5f994309ab8644f10fe945
Roman Chervontsev <roma.chervontsev@gmail.com>
2017-03-27 14:03:29 +0300

    добавление метрики

--- application/Views/Krujeva/Layouts/MainMobile.php
+++ application/Views/Krujeva/Layouts/MainMobile.php
@@ -154,6 +154,42 @@
 		['includeJS', '/public/js/App/Application.js'],
 
 	]); ?>
+	<!-- Yandex.Metrika informer -->
+	<a href="https://metrika.yandex.ru/stat/?id=43756824&amp;from=informer"
+	   target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/43756824/3_0_FFFFFFFF_EFEFEFFF_0_pageviews"
+										   style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="43756824" data-lang="ru" ><a>
+			<!-- /Yandex.Metrika informer -->
+
+			<!-- Yandex.Metrika counter -->
+			<script type="text/javascript">
+				(function (d, w, c) {
+					(w[c] = w[c] || []).push(function() {
+						try {
+							w.yaCounter43756824 = new Ya.Metrika({
+								id:43756824,
+								clickmap:true,
+								trackLinks:true,
+								accurateTrackBounce:true,
+								webvisor:true,
+								ecommerce:"dataLayer"
+							});
+						} catch(e) { }
+					});
+
+					var n = d.getElementsByTagName("script")[0],
+						s = d.createElement("script"),
+						f = function () { n.parentNode.insertBefore(s, n); };
+					s.type = "text/javascript";
+					s.async = true;
+					s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+					if (w.opera == "[object Opera]") {
+						d.addEventListener("DOMContentLoaded", f, false);
+					} else { f(); }
+				})(document, window, "yandex_metrika_callbacks");
+			</script>
+			<noscript><div><img src="https://mc.yandex.ru/watch/43756824" style="position:absolute; left:-9999px;" alt="" ><div></noscript>
+			<!-- /Yandex.Metrika counter -->
 	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
 	<script type="text/javascript">var isMobilePage = true;</script>
 </body>
